62b357d94e10f5aa5465fc2b934b11e680f4682f
Jennah <contactjennah@gmail.com>
2017-12-05 20:32:29 +0000

    Add player character images to resources.load

--- js/engine.js
+++ js/engine.js
@@ -223,7 +223,11 @@ var Engine = (function(global) {
         'images/char-boy.png',
         'images/small-heart.png',
         'images/game-over.png',
-        'images/Star.png'
+        'images/Star.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
